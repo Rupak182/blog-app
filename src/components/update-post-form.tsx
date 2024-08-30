ed4e7aa59eabe0ed09dc9a116663dbd83d3899bf
Rupak182 <rupakgoura18@gmail.com>
2024-08-30 16:34:49 +0530

    fixed update

--- src/components/update-post-form.tsx
+++ src/components/update-post-form.tsx
@@ -31,7 +31,7 @@ export default function UpdatePostForm({post}:postType) {
   const [state,formAction] = useFormState(editPost,initialState)
   const {title , slug ,body} = post
   return (
-    <>    <form action={formAction} className="flex flex-col max-w-[400px] mx-auto  space-x-2 mt-10 gap-2 ">
+    <>    <form action={formAction} className="flex flex-col w-10/12  mx-auto  space-x-2 mt-10 gap-2 ">
     <input defaultValue={title} className="border rounded  px-3  h-10 " type="text" name="title" placeholder="Title for new post" required />
     <textarea defaultValue={body} name="body" placeholder="Body content for new post" rows={6} className="border rounded px-3 required py-2" />
     <input type='hidden' name='slug' value={slug}/>
